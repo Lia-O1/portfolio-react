01af7e51c93dc7b07f542f765aeace7fbf29d556
Lia-O1 <133197997+Lia-O1@users.noreply.github.com>
2023-12-05 03:10:48 +1100

    Created images, views folders

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import Home from "./views/Home";
+import About from "./views/About";
+import Contact from "./views/Contact";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<About />} />
+          <Route path="/reservations" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
