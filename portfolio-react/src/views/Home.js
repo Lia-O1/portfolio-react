2ca4c36e84583e218c090f270db801d55ac81a69
Lia-O1 <133197997+Lia-O1@users.noreply.github.com>
2023-12-05 06:40:41 +1100

    About page structure&styled

--- portfolio-react/src/views/Home.js
+++ portfolio-react/src/views/Home.js
@@ -10,17 +10,20 @@ export default function Home() {
   return (
     <div className="Home">
       <Navbar />
-      <div class="d-flex flex-column ustify-content-center align-items-center text-center">
+      <div className="d-flex flex-column ustify-content-center align-items-center text-center">
         <h2>Hi, I am</h2>
         <h1>Olha Stepko</h1>
         <h3>Front-end developer, based in Australia</h3>
-        <div class="d-flex justify-content-center text-center mt-4 buttons">
-          <button href="/about-me.html" class="primary-btn rounded-2 me-3">
+        <div className="d-flex justify-content-center text-center mt-4 buttons">
+          <button href="/about-me.html" className="primary-btn rounded-2 me-3">
             <NavLink to="/contact" className="link">
               About me
             </NavLink>
           </button>
-          <button href="/contact-me.html" class="secondary-btn rounded-2 ms-3">
+          <button
+            href="/contact-me.html"
+            className="secondary-btn rounded-2 ms-3"
+          >
             <NavLink to="/contact" className="link">
               Contact me
             </NavLink>
@@ -28,54 +31,54 @@ export default function Home() {
         </div>
       </div>
       <div className="space py-5 ">
-        <div class="d-flex flex-column between-parts">
-          <div class="d-flex justify-content-start">
+        <div className="d-flex flex-column between-parts">
+          <div className="d-flex justify-content-start">
             Take a look at my portfolio - projects
           </div>
         </div>
       </div>
       <div className="py-5">
-        <div class="container mt-5">
-          <div class="row">
-            <div class="col-md-6 mb-3 mt-3">
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-md-6 mb-3 mt-3">
               <a
                 href="https://sparkling-sundae-e35163.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
               >
-                <div class="d-flex flex-column b-shadow rounded-2 project ">
-                  <div class="w-100 d-flex justify-content-center align-items-center p-5 pb-1">
-                    <div class="image-transition">
+                <div className="d-flex flex-column b-shadow rounded-2 project ">
+                  <div className="w-100 d-flex justify-content-center align-items-center p-5 pb-1">
+                    <div className="image-transition">
                       <img
                         src={tsukiNoYoru}
                         alt="Tsuki No Yoru Preview"
-                        class="b-shadow rounded-2 w-100"
+                        className="b-shadow rounded-2 w-100"
                       />
                     </div>
                   </div>
-                  <div class="d-flex justify-content-start p-3">
+                  <div className="d-flex justify-content-start p-3">
                     Tsuki No Yoru
                   </div>
                 </div>
               </a>
             </div>
-            <div class="col-md-6 mb-3 mt-3">
+            <div className="col-md-6 mb-3 mt-3">
               <a
                 href="https://jovial-bienenstitch-a81bd9.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
               >
-                <div class="d-flex flex-column rounded-2 project b-shadow">
-                  <div class="w-100 d-flex justify-content-center align-items-center p-5 pb-1">
-                    <div class="image-transition">
+                <div className="d-flex flex-column rounded-2 project b-shadow">
+                  <div className="w-100 d-flex justify-content-center align-items-center p-5 pb-1">
+                    <div className="image-transition">
                       <img
                         src={weatherToday}
                         alt="WeatherToday Preview"
-                        class="b-shadow rounded-2 w-100"
+                        className="b-shadow rounded-2 w-100"
                       />
                     </div>
                   </div>
-                  <div class="d-flex justify-content-start p-3">
+                  <div className="d-flex justify-content-start p-3">
                     WeatherToday
                   </div>
                 </div>
